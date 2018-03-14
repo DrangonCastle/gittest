ebd10cc0886c68b78814ea31e8df16ce74aa4bec
erwin <erwin@naver.com>
2018-03-14 15:39:33 +0900

    create entity class ver 1.0

--- gitProject/src/com/yang/entity/UserDao.java
+++ gitProject/src/com/yang/entity/UserDao.java
@@ -1,10 +1,37 @@
 package com.yang.entity;
 
+//기본 생성자, 인자3개 받는 생성자, setter&getter
 public class UserDao {
-
-	public static void main(String[] args) {
-		System.out.println("할로우 월드");
-
+	private String name;
+	private int age;
+	private double score;
+	
+	public UserDao() {
+		this.super();
 	}
-
+	
+	public UserDao(String name,int age,double score) {
+		this.name = name;
+		this.age = age;
+		this.score = score;
+	}
+	
+	public void GetName() {
+		return name;
+	}
+	public int GetAge() {
+		return age;
+	}
+	public int GetScore() {
+		return score;
+	}
+	public void SetName(String name) {
+		this.name = name;
+	}
+	public void SetAge(int age) {
+		this.age = age;
+	}
+	public void SetScore(int score) {
+		this.score = score;
+	}
 }

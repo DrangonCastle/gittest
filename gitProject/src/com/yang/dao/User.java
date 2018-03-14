5431d5dbaaf34a4fc6247bc774b172bd407541e9
yongsung <yongsung@naver.com>
2018-03-14 15:30:06 +0900

    ver 1

---
+++ gitProject/src/com/yang/dao/User.java
@@ -0,0 +1,10 @@
+package com.yang.dao;
+
+public class User {
+
+	public static void main(String[] args) {
+		System.out.println("Çï·Î¿ì ¿ùµå");
+
+	}
+
+}
---
+++ gitProject/src/com/yang/entity/UserDao.java
@@ -0,0 +1,10 @@
+package com.yang.entity;
+
+public class UserDao {
+
+	public static void main(String[] args) {
+		System.out.println("할로우 월드");
+
+	}
+
+}
